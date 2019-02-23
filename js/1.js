abca202813d9a728019c1bf8930d20d95eb61138
phamtrung14 <phamvantrung14111994@gmail.com>
2019-02-23 23:21:25 +0700

    trung

--- js/1.js
+++ js/1.js
@@ -1,3 +1,10 @@
  $(function(){
- 
+ 	$('.nd-khoi').slideUp();
+ 	//click vào thẻ
+ 	$('.mot-khoi').click(function(event) {
+ 		/* Act on the event */
+ 		console.log('da click');
+ 		$('.nd-khoi').slideToggle();
+ 		$('.mot-khoi').addClass('trang')
+ 	});
 })  
