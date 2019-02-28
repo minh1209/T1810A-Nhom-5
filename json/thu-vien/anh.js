71ba99113593e5d6425cabc4b4517fa678e498dd
phamtrung14 <phamvantrung14111994@gmail.com>
2019-02-28 16:24:33 +0700

    trung

---
+++ json/thu-vien/anh.js
@@ -0,0 +1,36 @@
+var anh = [
+		{
+			id_anh :'I0001',
+			tat_ca :'images/sinh-nhat.png'
+		},
+		
+		{
+			id_anh :'I0002',
+			tat_ca :'images/sinh-nhat.png'
+		},
+
+		{
+			id_anh :'I0002',
+			tat_ca :'images/sinh-nhat.png'
+		}
+];
+var g5_fans =[
+	{
+		id_g5_fans:'G0001',
+		g5_img:'images/sinh-nhat.png'
+	},
+	{
+		id_g5_fans:'G0002',
+		g5_img:'images/sinh-nhat.png'
+	},
+	{
+		id_g5_fans:'G0002',
+		g5_img:'images/sinh-nhat.png'
+	}
+];
+var sn =[
+	{
+		id_sn:'S0001',
+		sn_img:'images/sinh-nhat.png'
+	}
+]
---
+++ json/thu-vien/loai_anh.js
@@ -0,0 +1,14 @@
+var loai_anh=[
+	{
+		id_loai_anh:"A0001",
+		loai_anh:"tat-ca"
+	}
+	{
+		id_loai_anh:"A0002",
+		loai_anh:"g5-fans"
+	}
+	{
+		id_loai_anh:"A0003",
+		loai_anh:"g5-sn"
+	}
+]
