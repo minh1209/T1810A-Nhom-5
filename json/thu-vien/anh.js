c8d40f026757f84d41edd185d80a66d69062b2fe
phamtrung14 <phamvantrung14111994@gmail.com>
2019-03-05 14:07:59 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	json/san_pham.js

--- json/thu-vien/anh.js
+++ json/thu-vien/anh.js
@@ -1,7 +1,7 @@
 var anh = [
 		{
 			id_anh :'I0001',
-			tat_ca :'images/sinh-nhat.png'
+			tat_ca :'images/thanh-vien/2.jfif'
 		},
 		
 		{
@@ -10,22 +10,42 @@ var anh = [
 		},
 
 		{
-			id_anh :'I0002',
-			tat_ca :'images/sinh-nhat.png'
+			id_anh :'I0003',
+			tat_ca :'images/thanh-vien/1.jfif'
+		},
+		{
+			id_anh :'I0004',
+			tat_ca :'images/thanh-vien/3.jfif'
+		},
+		{
+			id_anh :'I0005',
+			tat_ca :'images/thanh-vien/4.jfif'
+		},
+		{
+			id_anh :'I0006',
+			tat_ca :'images/thanh-vien/5.jfif'
 		}
 ];
 var g5_fans =[
 	{
 		id_g5_fans:'G0001',
-		g5_img:'images/sinh-nhat.png'
+		g5_img:'images/thanh-vien/5.jfif'
 	},
 	{
 		id_g5_fans:'G0002',
-		g5_img:'images/sinh-nhat.png'
+		g5_img:'images/thanh-vien/4.jfif'
 	},
 	{
-		id_g5_fans:'G0002',
-		g5_img:'images/sinh-nhat.png'
+		id_g5_fans:'G0003',
+		g5_img:'images/thanh-vien/3.jfif'
+	},
+	{
+		id_g5_fans:'G0004',
+		g5_img:'images/thanh-vien/2.jfif'
+	},
+	{
+		id_g5_fans:'G0005',
+		g5_img:'images/thanh-vien/1.jfif'
 	}
 ];
 var sn =[
